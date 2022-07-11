de1c1f7803755c0a9f5c77d26d97fe79d91eedcd
ubuntu_imx <2282231661@qq.com>
2022-07-11 07:12:27 -0700

    1. add imx6 linux kernelw

---
+++ linux-imx-rel_imx_4.1.15_2.1.0_ga/make.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#make clean
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_v7_defconfig
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j16
